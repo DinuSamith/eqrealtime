f6bffe743bae5eda96ebd3b7c6541a5c23d5d9d7
DinuSamith <111166494+DinuSamith@users.noreply.github.com>
2025-04-15 12:36:11 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("eqrealtime-cache").then(function (cache) {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("eqrealtime-cache").then((cache) => {
       return cache.addAll(["index.html", "manifest.json"]);
     })
   );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
